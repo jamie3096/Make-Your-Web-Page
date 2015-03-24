a7709fdea8978e0f9bafe841a2ea63f8c7615210
jhloden <jhloden@bellsouth.net>
2015-03-23 20:38:29 -0400

    Stage 3 notes

--- html_generator.py
+++ html_generator.py
@@ -2,7 +2,7 @@
 def HTML_generator(description):
     html_text_1 = '''
 <div class="notes">
-    <div class="Stage_2">
+    <div class="Stage_3">
         ''' + description
     html_text_2 = '''
     </div>
@@ -15,15 +15,17 @@ def make_HTML(concept):
     return HTML_generator(description)
 
 # Notes that will be presented in the HTML webpage.
-notes_list = [ ['Python - Python is a programming language.  A <i>Python Interpreter</i> converts the programming code as a set of instructions for the computer to understand.'],
-               ['An <b>expression</b> is something that has value.  To introduce a new value use an <b>assignment statement</b>.  Ex.... Name=Expression'],
-               ['<b>Functions</b> - a function is a block of reusable code that is used to perform an action.  You can defind a function to provide added functionality.  Function blocks begin with the keyword <b>def</b> followed by parentheses() and any input paramenter should be put withing these parentheses.  Ex.... <b>def</b> <i>function (paramenter):</i>'],
-               ['<b>Variables</b> - is a name that refers to a value.  Variables store data and allow us to write the code in a way that makes it understandable. We can assign a value to the variable and be able to manipulate the value when needed.'],
-               ['For Loops allow you to iterate over list or a string.'],
-               ['<b>Strings</b> - a string is a sequence of characters surrounded by quotes.  You can use operators on stringes such as + and *.  These operators have a different meaning in strings than intergers, with strings it means concatenate.'],
-               ['Indexing a string is selecting a sub-sequence from the string you can do this by using square brackets[] on the part of the string you want to select.'],
-               ['You can use find to locate information within a string. Ex....<i>search string</i>.<b>find</b>(<i>target string</i>).  You can also put in a number to search from a certain position of the string.  Ex....<i>searchstring</i>.<b>find</b>(<i>target string, number</i>)'],
-               ['<b>Lists</b> - list is a datatype that can be written as a list of comma-seperated values between square brackets. List are very versatile and can be indexed, updated, and mutated.'] ]
+notes_list = [ ['OOP - <b>Object Oriented Programming</b> - is about reusing code. In stead of changing code or starting from scratch it is easier to customize the code that already exist therefore minimizing redundancy.'],
+               ['Modules package program code and data for reuse.'],
+               ['Classes - classes are designed to create new objects that support inheritance.  Classes are created by using the statement <i>"class"</i>.  Classes are always inside modules.'],
+               ['Instances - instances are created by calling class as if it were a function.  Each instance object inherits class attributes.'],
+               ['Normally all instance attributes are initialized in the __init__constructor method.'],
+               ['Attributes are usually attached to instance by assignments to the argument passed to functions coded inside classes, called self.'],
+               ['The __init__ method is known as the constructor because of when it is run.'],
+               ['Inheritance chooses the first occurrence of an attribute it finds when an attribute is referenced normally - by self.method() for example.'], 
+               ['Class Variables are associated within a Class.'],
+               ['Loops - code loops allow you to read one or more inputs.'],
+               ['Python comes with a collection of prebuilt functionality known as the standard library. The standard library supports many different types of programming task.'] ]
 
 def HTML_from_notes_list(list_of_concepts):
     HTML = ""
