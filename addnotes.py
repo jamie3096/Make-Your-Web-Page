7e5e6efa027e54eb5943c9a6bc619791adbc8a81
jhloden <jhloden@bellsouth.net>
2015-05-04 19:47:55 -0400

    Stage 5 Submission

---
+++ addnotes.py
@@ -0,0 +1,28 @@
+import os
+import webapp2
+import jinja2
+import urllib
+
+#template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),autoescape = True)
+#jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        items = self.request.get_all("notes")
+        self.render("Add_Notes.html", items = items)
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ],
+                              debug=True)
