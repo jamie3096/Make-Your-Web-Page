0b889f9c6f36b42858b18b6bc820b774a89ee66c
jhloden <jhloden@bellsouth.net>
2015-05-04 19:54:00 -0400

    deleted unwanted code

--- untitled folder/addnotes.py
+++ untitled folder/addnotes.py
@@ -3,8 +3,9 @@ import webapp2
 import jinja2
 import urllib
 
+#template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),autoescape = True)
-
+#jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
